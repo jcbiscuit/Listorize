dbbe6c093e5e142ba2922500906494dc8eb1555a
Jody Mazinyi <jodyclelland8@gmail.com>
2021-02-16 11:42:49 +1100

    Initial Commit

---
+++ ListorizeSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ListorizeSwiftUI
+//
+//  Created by Jody Clelland on 16/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ListorizeSwiftUI/ListorizeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListorizeSwiftUIApp.swift
+//  ListorizeSwiftUI
+//
+//  Created by Jody Clelland on 16/2/21.
+//
+
+import SwiftUI
+
+@main
+struct ListorizeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
