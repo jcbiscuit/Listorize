af504e92a82d937a7e732cda3c21aa38fa79e9bb
Jody Mazinyi <jodyclelland8@gmail.com>
2021-02-23 11:35:55 +1100

    removal of core data

--- ListorizeSwiftUI/Views/AddToDoView.swift
+++ ListorizeSwiftUI/Views/AddToDoView.swift
@@ -11,53 +11,74 @@ import CoreData
 struct AddToDoView: View {
     
     @Environment(\.presentationMode) var presentationMode
-    @Environment(\.managedObjectContext) var managedObjectContext
+    //    @Environment(\.managedObjectContext) var managedObjectContext
     
     
     @State private var name: String = ""
     @State private var errorShowing: Bool = false
     @State private var errorTitle: String = ""
     @State private var errorMessage: String = ""
-
-
-    func addNewItem() {
-        
-        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
-            return
-        }
-        
-        let managedContext = appDelegate.persistentContainer.viewContext
-        
-        let entity = NSEntityDescription.entity(forEntityName: "Saving", in: managedContext)!
-        
-        let newToDo = NSManagedObject(entity: entity, insertInto: managedContext)
-        
-        newToDo.setValue($name.wrappedValue, forKeyPath: "stringAttribute")
-        
-        _ = Saving(context: self.managedObjectContext)
+    
+    //
+    //    func addNewItem() {
+    //
+    //        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+    //            return
+    //        }
+    //
+    //        let managedContext = appDelegate.persistentContainer.viewContext
+    //
+    //        let entity = NSEntityDescription.entity(forEntityName: "Saving", in: managedContext)!
+    //
+    //        let newToDo = NSManagedObject(entity: entity, insertInto: managedContext)
+    //
+    //        newToDo.setValue($name.wrappedValue, forKeyPath: "stringAttribute")
+    //
+    //        _ = Saving(context: self.managedObjectContext)
+    //
+    //
+    //
+    //    }
+    
+    //    func loadSaved() {
+    //        guard let appDelegate =
+    //                UIApplication.shared.delegate as? AppDelegate else {
+    //            return
+    //        }
+    //
+    //        let managedContext =
+    //            appDelegate.persistentContainer.viewContext
+    //
+    //        let fetchRequest =
+    //        NSFetchRequest<NSManagedObject>(entityName: "Saving")
+    //
+    //            do {
+    //               _ = try managedContext.fetch(fetchRequest)
+    //            } catch let error as NSError {
+    //                print("Could not fetch. \(error), \(error.userInfo)")
+    //            }
+    //
+    //    }
+    //        do {
+    //            try managedContext.save()
+    //            print("saved successfully -- \($name.wrappedValue)")
+    //        } catch let error as NSError {
+    //            print("Could not save. \(error), \(error.userInfo)")
+    //        }
     
     
-    }
-//        do {
-//            try managedContext.save()
-//            print("saved successfully -- \($name.wrappedValue)")
-//        } catch let error as NSError {
-//            print("Could not save. \(error), \(error.userInfo)")
-//        }
-    
-
     var body: some View {
         
-            VStack {
-                NavigationView {
-            
+        VStack {
+            NavigationView {
                 Form {
                     
                     TextField("Listorize Today", text: $name)
-                
+                    
                     Button(action: {
                         if self.name != "" {
-                            addNewItem()
+                            //                            addNewItem()
+                            
                         } else {
                             self.errorShowing = true
                             self.errorTitle = "Oops!"
@@ -65,14 +86,13 @@ struct AddToDoView: View {
                             return
                         }
                         self.presentationMode.wrappedValue.dismiss()
-                    
+                        
+                        
                     }) {
                         Text("Save")
                     }
                     
-                    
-                
-                    }
+                }
                 
                 .navigationBarTitle("TODAY", displayMode: .inline)
                 .navigationBarItems(trailing:
@@ -81,29 +101,26 @@ struct AddToDoView: View {
                                         }) {
                                             Image(systemName: "xmark")
                                                 .font(.system(size: 20, weight:.bold))
-                                        
+                                            
                                         }
                 )
             }
-    
-    
+            
             .alert(isPresented: $errorShowing) {
                 Alert(title: Text(errorTitle), message: Text(errorMessage), dismissButton: .default(Text("Go it, thanks!")))
             }
-        
-            
         }
     }
-
-
-struct AddToDoView_Previews: PreviewProvider {
-    static var previews: some View {
-        AddToDoView()
-        AddToDoView()
-            .preferredColorScheme(.light)
+    
+    
+    struct AddToDoView_Previews: PreviewProvider {
+        static var previews: some View {
+            AddToDoView()
+            AddToDoView()
+                .preferredColorScheme(.light)
+        }
     }
-}
-
+    
 }
 
 
--- ListorizeSwiftUI/Views/TodayView.swift
+++ ListorizeSwiftUI/Views/TodayView.swift
@@ -13,68 +13,69 @@ struct TodayView: View {
     
     @Environment(\.managedObjectContext) var managedObjectContext
     
+    
     @ObservedObject var taskStore = TaskStore()
     @State var newToDo: String = ""
     @State var open = false
     @State private var showingAddToDoView: Bool = false
     
-//    @State var priority: String = "Normal"
+    //    @State var priority: String = "Normal"
     
-//    let priorities = ["High", "Normal", "Low"]
+    //    let priorities = ["High", "Normal", "Low"]
     
     
-//    var searchBar: some View {
-//        Text("")
-//        TextField("Listorize Today", text: self.$newToDo)
-//            .padding()
-//            .foregroundColor(Color("TextColor"))
-//            .background(Color.white)
-//            .cornerRadius(50)
-//    }
-//
+    //    var searchBar: some View {
+    //        Text("")
+    //        TextField("Listorize Today", text: self.$newToDo)
+    //            .padding()
+    //            .foregroundColor(Color("TextColor"))
+    //            .background(Color.white)
+    //            .cornerRadius(50)
+    //    }
+    
+    
+    //
     func addNewToDo() {
         taskStore.tasks.append(Task(id: String(taskStore.tasks.count + 1), toDoItem: newToDo))
         self.newToDo = ""
     }
     
-    
     var body: some View {
         
         VStack {
-            HStack{
-//                    searchBar.padding()
+            //                    searchBar.padding()
+            
+            Button(action: {
+                self.showingAddToDoView.toggle()
+            }) {
+                Image(systemName: "plus")
+                    .foregroundColor(Color(.systemBlue))
+                    .font(.system(size: 30, weight:.bold))
                 
-                Button(action: {
-                    self.showingAddToDoView.toggle()
-                }) {
-                    Image(systemName: "plus")
-                        .foregroundColor(Color(.systemBlue))
-                        .font(.system(size: 30, weight:.bold))
-                        
-                }
-                
-                .sheet(isPresented: $showingAddToDoView) {
-                    AddToDoView().environment(\.managedObjectContext, self.managedObjectContext)
-                }
-                
-                
-//                .background(Color(.systemBlue))
-                .mask(Circle().fill())
-                .padding(.all)
             }
             
-//            Picker("Priority", selection: $priority) {
-//                ForEach(priorities, id: \.self) {
-//                    Text($0)
-//                }
-//            }
+            .sheet(isPresented: $showingAddToDoView) {
+                AddToDoView().environment(\.managedObjectContext, self.managedObjectContext)
+            }
+            //                .background(Color(.systemBlue))
+            .mask(Circle().fill())
+            .padding()
+            
+            
+            //            Picker("Priority", selection: $priority) {
+            //                ForEach(priorities, id: \.self) {
+            //                    Text($0)
+            //                }
+            //            }
             .pickerStyle(SegmentedPickerStyle())
             
             Form {
                 ForEach(self.taskStore.tasks) { task in
                     Text(task.toDoItem)
+                    
                 }.onMove(perform: self.move)
                 .onDelete(perform: self.delete)
+                
             }
             
             .navigationBarTitle("TODAY", displayMode: .inline)
@@ -83,6 +84,7 @@ struct TodayView: View {
             )
         }
     }
+    
     
     
     func move(from soure: IndexSet, to destination: Int) {
@@ -98,7 +100,10 @@ struct TodayView_Previews: PreviewProvider {
     static var previews: some View {
         TodayView()
         TodayView()
-            .preferredColorScheme(.dark)
+        
+        //        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        //        return TodayView()
+        //            .environment(\.managedObjectContext, context)
     }
 }
 
